f63428d48d16d08a3a972904d10b96810889dd54
Vibhor Garg <60567317+vibhorgrg3@users.noreply.github.com>
2021-02-23 11:53:53 +0530

    Day1 assignment done(22/02/2021)

---
+++ Day1.py
@@ -0,0 +1,15 @@
+#Q1 Printing addition, subtraction, multiplication
+#and division of given 2 numbers
+
+x=int(input("Enter number 1: "))
+y=int(input("Enter number 2: "))
+print("SUM: ",x+y,"\nDifference: ",x-y,"\nProduct: ",x*y,"\nDivision: ",x/y)
+print("\n")
+
+
+
+#Q2 Printing number 1 raised to power of number 2
+
+x=int(input("Enter number 1: "))
+y=int(input("Enter number 2: "))
+print("X^Y = ", x**y)
